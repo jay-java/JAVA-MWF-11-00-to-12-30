9448395e8035528af972b29780f66dccc5897358
jay-java <jayprajapati.tops@gmail.com>
2024-10-02 12:39:45 +0530

    spring mvc fetch update

--- SpringMVC/src/main/java/com/controller/UserController.java
+++ SpringMVC/src/main/java/com/controller/UserController.java
@@ -9,14 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.dao.UserDao;
 import com.model.User;
 
 @Controller
 public class UserController {
+	
 
 //	@RequestMapping("/")
 //	public String indexPage(HttpServletRequest request) {
@@ -34,26 +38,23 @@ public class UserController {
 //		return "index";
 //	}
 	
-	@RequestMapping("/")
-	public String indexPage(Model m) {
-		m.addAttribute("contact", 987654321);
-		m.addAttribute("name", "JAVA");
-		List list = new ArrayList();
-		list.add(1);
-		list.add("java");
-		list.add(1234.34);
-		list.add(false);
-		list.add('d');
-		list.add(2343423);
-		list.add("python");
-		m.addAttribute("list", list);
-		return "index";
-	}
+//	@RequestMapping("/")
+//	public String indexPage(Model m) {
+//		m.addAttribute("contact", 987654321);
+//		m.addAttribute("name", "JAVA");
+//		List list = new ArrayList();
+//		list.add(1);
+//		list.add("java");
+//		list.add(1234.34);
+//		list.add(false);
+//		list.add('d');
+//		list.add(2343423);
+//		list.add("python");
+//		m.addAttribute("list", list);
+//		return "index";
+//	}
 	
-	@RequestMapping("/home")
-	public String home() {
-		return "home";
-	}
+	
 	
 //	@RequestMapping("/register")
 //	public void registerUser(HttpServletRequest request) {
@@ -71,12 +72,42 @@ public class UserController {
 //		System.out.println(name + contact + address);
 //	}
 	
+	
 	@Autowired
 	private UserDao dao;
 	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	
+	@RequestMapping("/home")
+	public String home(Model m) {
+		List<User> list = this.dao.getAllUser();
+		m.addAttribute("list",list);
+		return "home";
+	}
+	
+	
 	@RequestMapping("/register")
-	public void registerUser(@ModelAttribute User u) {
+	public String registerUser(@ModelAttribute User u,Model m) {
 		System.out.println(u);
 		this.dao.insertOrUpdateUser(u);
+		List<User> list = this.dao.getAllUser();
+		m.addAttribute("list",list);
+		return "home";
+	}
+	
+	@RequestMapping(value = "/edit/{id}")
+	public ModelAndView editUser(@PathVariable("id") int id) {
+		System.out.println("edit user called");
+		User u  = this.dao.getUserById(id);
+		System.out.println(u);
+//		m.addAttribute("u",u);
+		ModelAndView m = new ModelAndView();
+		m.addObject("u",u);
+		m.setViewName("update");
+		return m;
 	}
 }
--- SpringMVC/src/main/webapp/WEB-INF/views/update.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/views/update.jsp
@@ -24,36 +24,42 @@
 	<h1 class="text-center">Hello, world!</h1>
 	<div class="container">
 	<form action="register" method="post">
+			<div class="mb-3">
+			<label for="exampleInputEmail1" class="form-label">Id </label> <input
+				type="text" name="name" value="${u.id }" class="form-control" id="exampleInputEmail1"
+				aria-describedby="emailHelp">
+
+		</div>
 		<div class="mb-3">
 			<label for="exampleInputEmail1" class="form-label">Name </label> <input
-				type="text" name="name" class="form-control" id="exampleInputEmail1"
+				type="text" name="name" value="${u.name }" class="form-control" id="exampleInputEmail1"
 				aria-describedby="emailHelp">
 
 		</div>
 		<div class="mb-3">
 			<label for="exampleInputEmail1" class="form-label">Contact </label> <input
-				type="text" name="contact" class="form-control"
+				type="text" name="contact" value="${u.contact }" class="form-control"
 				id="exampleInputEmail1" aria-describedby="emailHelp">
 
 		</div>
 		<div class="mb-3">
 			<label for="exampleInputEmail1" class="form-label">Address </label> <input
-				type="text" name="address" class="form-control"
+				type="text" name="address" value="${u.address }" class="form-control"
 				id="exampleInputEmail1" aria-describedby="emailHelp">
 
 		</div>
 		<div class="mb-3">
 			<label for="exampleInputEmail1" class="form-label">Email
-				address</label> <input type="email" name="email" class="form-control"
+				address</label> <input type="email" name="email" value="${u.email }" class="form-control"
 				id="exampleInputEmail1" aria-describedby="emailHelp">
 
 		</div>
 		<div class="mb-3">
 			<label for="exampleInputPassword1" class="form-label">Password</label>
-			<input type="password" name="password" class="form-control"
+			<input type="password" name="password" value="${u.password }" class="form-control"
 				id="exampleInputPassword1">
 		</div>
-		<button type="submit" class="btn btn-primary">Register</button>
+		<button type="submit" class="btn btn-primary">Update</button>
 	</form>
 	
 	<div class="text-center"><a href="home">Home</a></div>
@@ -62,45 +68,3 @@
 
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-<%-- <!DOCTYPE html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-	hello index page
-	<h1>
-		<a href="home">Home</a>
-	</h1>
-	 <%
- int contact= (int)request.getAttribute("contact");
- String name = (String)request.getAttribute("name");
- List list = (List)request.getAttribute("list");
- %>
- 
- <h1><%out.print(contact); %></h1>
- <h1><%out.print(name); %></h1>
- <h1><%out.print(list); %></h1>
- 
-
-	<h1>${contact }</h1>
-	<h1>${name }</h1>
-	<h1>${list }</h1>
-	<c:forEach items="${list }" var="u">
-		<h2>${u }</h2>
-	</c:forEach>
-	
-	
-
-</body>
-</html> --%>
